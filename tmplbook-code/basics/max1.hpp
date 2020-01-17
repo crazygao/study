3dd15540f1fe156ec2e5633ba8e3b8f14cdd3b6d
Philip Gao <yigao@microsoft.com>
2020-01-17 18:55:01 +0800

    Add modified tmplbook-code

---
+++ tmplbook-code/basics/max1.hpp
@@ -0,0 +1,9 @@
+// Function template
+template<typename T>
+T max (T a, T b)
+{
+  // if b < a then yield a else yield b
+  return  b < a ? a : b;
+  // indicate T is comparable,
+  // indicate T is copiable.
+}
