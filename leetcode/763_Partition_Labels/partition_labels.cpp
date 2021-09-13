80a2461ac112b22b7c6a1fb52858c4d89439baa4
Philip Gao <yigao@microsoft.com>
2021-09-13 17:54:33 +0800

    More edit on partition_labels

--- leetcode/763_Partition_Labels/partition_labels.cpp
+++ leetcode/763_Partition_Labels/partition_labels.cpp
@@ -1,8 +1,19 @@
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 class Solution{
     public:
+        // 2. letter to vector<index>
+        // 3. leftoverLetterIndexs
+        unordered_map<char, vector<int>> partitions;
+        typedef unsigned index;
+        void initPartitions(string s) {
+            for (auto it = s.cbegin(); it != s.cend(); it++) {
+                int distance = it - s.cbegin();
+
+            }
+        }
         vector<int> partitionLabels(string s) {
             
         }
